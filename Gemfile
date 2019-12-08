cd0eb0a1a60403bbb2898d923a390475a22e2e5f
logiteca7 <aaaa777@users.noreply.github.com>
2019-12-08 13:33:54 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.4.4'
+
+gem 'rbnacl', '~> 6.0'
+gem 'discordrb', :github => 'discordrb/discordrb'#'3.1.1'
+#gem 'pg'
+#gem 'rbnacl-libsodium'
---
+++ main.rb
@@ -0,0 +1,61 @@
+#require './defs'
+require 'discordrb'
+require 'open3'
+#require 'pg'
+require 'net/https'
+require 'open-uri'
+
+A7ID = 238575590456688641
+bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], client_id: 374072997729796109, name: 'test_bot', prefix: '!', help_command: :hep
+
+bot.ready do |e|
+  #youtube_url = "https://www.youtube.com/watch?v=Jo1tjBmwuXI"
+  #puts "YouTube URL: #{youtube_url}"
+  #`mkdir youtube`
+  #Dir.chdir "youtube"
+  #`curl https://yt-dl.org/downloads/latest/youtube-dl > youtube-dl`
+  #`chmod 755 youtube-dl`
+  #`./youtube-dl "#{youtube_url}" -o video.mp4`
+  #`git clone https://code.google.com/p/plowshare/ plowshare4; cd plowshare4; make install PREFIX=/app/youtube`
+  #puts `./bin/plowup zippyshare video.mp4`
+end
+
+#bot.command(:new) do |e, *args|
+#  uri = URI.parse(ENV['DATABASE_URL'])
+#  connection = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+#  connection.exec('drop table if exists music')
+#  connection.close
+#end
+
+bot.command(:con) do |e|
+  vcch = e.user.voice_channel
+  bot.voice_connect(vcch)
+  "connected to #{vcch.name}"
+end
+
+bot.command(:dc) do |e|
+  bot.voices[e.server.id].destroy
+  'bye'
+end
+
+bot.command(:play) do |e, *args|
+  voice_bot = bot.voices[e.server.id]
+  return 'you aren\'t in voice ch' unless voice_bot
+
+  url = args.first
+  
+  command = "youtube-dl #{url} -o - | ffmpeg -loglevel 0 -i - -f s16le -ar 48000 -ac 2 pipe:1"
+
+  IO.popen(command) do |encode_io|
+    voice_bot.play(encode_io)
+  end
+
+  'video end'
+end
+
+bot.command(:eval) do |e, *args|
+  return unless e.author.id == A7ID
+  eval(args.join(' '))
+end
+
+bot.run
